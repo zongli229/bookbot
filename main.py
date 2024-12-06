3a41f920995d8cfb1f0323a5154793b8f7598750
zongli229 <zongli229@gmail.com>
2024-12-06 01:34:08 -0500

    Created .gitignore for books/ and updated main.py to open up frankenstein.txt

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
