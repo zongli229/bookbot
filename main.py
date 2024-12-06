a3229a44c773ef3308a6de264cb7ac9f2c6c0d07
zongli229 <zongli229@gmail.com>
2024-12-06 02:27:07 -0500

    Added comments in code

--- main.py
+++ main.py
@@ -16,14 +16,16 @@ def main():
     
     print("--- End report ---")
 
-
+#This function takes the text and splits it into words and returns the count of words
 def get_num_words(text):
     words = text.split()
     return len(words)
 
+#This function orders the list by the character count
 def sort_on(d):
     return d["num"]
 
+#This function throws the character dictionary into a list with key value pairs
 def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list = []
     for ch in num_chars_dict:
@@ -31,8 +33,10 @@ def chars_dict_to_sorted_list(num_chars_dict):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
+#This function creates a dictionary of all the characters in the text
 def get_chars_dict(text):
     chars = {}
+    #converts the character to lowercase and counts it
     for c in text:
         lowered = c.lower()
         if lowered in chars:
@@ -41,6 +45,7 @@ def get_chars_dict(text):
             chars[lowered] = 1
     return chars
 
+#This function locates the book in the directory and opens it up
 def get_book_text(path):
     with open(path) as f:
         return f.read()
